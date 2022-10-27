ed148121120872eb05ce5e9472cbd67cc0e904f3
Lucas Vienna <dev@lucasvienna.dev>
2022-10-27 22:21:35 +0200

    Update API to WoW10, rework tooltip scanning

--- AdiBags_Bound/Main.lua
+++ AdiBags_Bound/Main.lua
@@ -30,111 +30,254 @@ SOFTWARE.
 	and partially based on his template: https://github.com/GoldpawsStuff/AddonTemplate
 --]]
 
-local _, ns = ...
+-- Retrive addon folder name, and our local, private namespace.
+local Addon, Private = ...
 
-local addon = LibStub('AceAddon-3.0'):GetAddon('AdiBags')
-local L = setmetatable({}, {__index = addon.L})
+-- AdiBags namespace
+-----------------------------------------------------------
+local AdiBags = LibStub("AceAddon-3.0"):GetAddon("AdiBags")
 
-do -- Localization
-  L["Bound"] = "Bound"
-  L["Put BOE/BOA in their own sections."] = "Put BOE/BOA in their own sections."
+-- Lua API
+-----------------------------------------------------------
+local _G = _G
+local string_find = string.find
 
-  local locale = GetLocale()
-  if locale == "frFR" then
 
-  elseif locale == "deDE" then
-    
-  elseif locale == "esMX" then
-    
-  elseif locale == "ruRU" then
-    
-  elseif locale == "esES" then
-    
-  elseif locale == "zhTW" then
-    
-  elseif locale == "zhCN" then
-    
-  elseif locale == "koKR" then
-    
-  end
+-- WoW API
+-----------------------------------------------------------
+local CreateFrame = _G.CreateFrame
+local GetLocale = _G.GetLocale
+local GetContainerItemInfo = _G.GetContainerItemInfo
+local GetItemInfo = _G.GetItemInfo
+
+-- WoW10 API
+-----------------------------------------------------------
+local C_Container_GetContainerItemInfo = C_Container and C_Container.GetContainerItemInfo
+
+-- WoW Strings
+-----------------------------------------------------------
+local S_ITEM_BOP = ITEM_SOULBOUND
+local S_ITEM_BOA = ITEM_ACCOUNTBOUND
+local S_ITEM_BOA2 = ITEM_BNETACCOUNTBOUND
+local S_ITEM_BOA3 = ITEM_BIND_TO_BNETACCOUNT
+local S_ITEM_BOE = ITEM_BIND_ON_EQUIP
+
+-- Constants
+-----------------------------------------------------------
+local S_BOA = "BoA"
+local S_BOE = "BoE"
+
+-- Localization system
+-----------------------------------------------------------
+-- Do not modify the function, 
+-- just the locales in the table below!
+local L = (function(tbl,defaultLocale) 
+	local gameLocale = GetLocale() -- The locale currently used by the game client.
+	local L = tbl[gameLocale] or tbl[defaultLocale] -- Get the localization for the current locale, or use your default.
+	-- Replace the boolean 'true' with the key,
+	-- to simplify locale creation and reduce space needed.
+	for i in pairs(L) do 
+		if (L[i] == true) then 
+			L[i] = i
+		end
+	end 
+	-- If the game client is in another locale than your default, 
+	-- fill in any missing localization in the client's locale 
+	-- with entries from your default locale.
+	if (gameLocale ~= defaultLocale) then 
+		for i,msg in pairs(tbl[defaultLocale]) do 
+			if (not L[i]) then 
+				-- Replace the boolean 'true' with the key,
+				-- to simplify locale creation and reduce space needed.
+				L[i] = (msg == true) and i or msg
+			end
+		end
+	end
+	return L
+end)({ 
+	-- ENTER YOUR LOCALIZATION HERE!
+	-----------------------------------------------------------
+	-- * Note that you MUST include a full table for your primary/default locale!
+	-- * Entries where the value (to the right) is the boolean 'true',
+	--   will use the key (to the left) as the value instead!
+	["enUS"] = {
+		["Bound"] = true,
+		["Put BoE and BoA items in their own sections."] = true,
+	},
+	["deDE"] = {},
+	["esES"] = {},
+	["esMX"] = {},
+	["frFR"] = {},
+	["itIT"] = {},
+	["koKR"] = {},
+	["ptPT"] = {},
+	["ruRU"] = {},
+	["zhCN"] = {},
+	["zhTW"] = {}
+	
+-- The primary/default locale of your addon.
+-- * You should change this code to your default locale.
+-- * Note that you MUST include a full table for your primary/default locale!
+}, "enUS")
+
+--------------------------------------------------------------------------------
+-- Filter Setup
+--------------------------------------------------------------------------------
+
+local filter = AdiBags:RegisterFilter("Bound", 92, "ABEvent-1.0")
+filter.uiName = L["Bound"]
+filter.uiDesc = L["Put BoE and BoA items in their own sections."]
+
+function filter:OnInitialize()
+	self.db = AdiBags.db:RegisterNamespace("Bound", {
+		profile = {
+			enableBoE = true,
+			enableBoA = true,
+		},
+	})
 end
 
-local tooltip
-local function create()
-  local tip, leftside = CreateFrame("GameTooltip"), {}
-  for i = 1,6 do
-    local L,R = tip:CreateFontString(), tip:CreateFontString()
-    L:SetFontObject(GameFontNormal)
-    R:SetFontObject(GameFontNormal)
-    tip:AddFontStrings(L,R)
-    leftside[i] = L
-  end
-  tip.leftside = leftside
-  return tip
+function filter:Update()
+	self:SendMessage("AdiBags_FiltersChanged")
 end
 
--- The filter itself
-
-local setFilter = addon:RegisterFilter("Bound", 92, 'ABEvent-1.0')
-setFilter.uiName = L['Bound']
-setFilter.uiDesc = L['Put BOE/BOA in their own sections.']
-
-function setFilter:OnInitialize()
-  self.db = addon.db:RegisterNamespace('Bound', {
-    profile = { enableBoE = true, enableBoA = true },
-    char = {  },
-  })
+function filter:OnEnable()
+	AdiBags:UpdateFilters()
 end
 
-function setFilter:Update()
-  self:SendMessage('AdiBags_FiltersChanged')
+function filter:OnDisable()
+	AdiBags:UpdateFilters()
 end
 
-function setFilter:OnEnable()
-  addon:UpdateFilters()
+--------------------------------------------------------------------------------
+-- Actual filter - with a cache
+--------------------------------------------------------------------------------
+
+--[[
+	Item Qualities
+
+	0 	Poor 		Poor
+	1 	Common 		Common
+	2 	Uncommon 	Uncommon
+	3 	Rare 		Rare
+	4 	Epic 		Epic
+	5 	Legendary 	Legendary
+	6 	Artifact 	Artifact
+	7 	Heirloom 	Heirloom
+	8 	WoWToken 	WoW Token
+]]
+
+--[[
+	Bind Types
+
+	0 	LE_ITEM_BIND_NONE
+	1 	LE_ITEM_BIND_ON_ACQUIRE 	Bind on Pickup
+	2 	LE_ITEM_BIND_ON_EQUIP 		Bind on Equip
+	3 	LE_ITEM_BIND_ON_USE 		Bind on Use
+	4 	LE_ITEM_BIND_QUEST
+]]
+
+
+-- Tooltip used for scanning.
+-- Let's keep this name for all scanner addons.
+local _SCANNER = "AVY_ScannerTooltip"
+local Scanner = _G[_SCANNER] or CreateFrame("GameTooltip", _SCANNER, WorldFrame, "GameTooltipTemplate")
+
+-- Cache of information objects,
+-- globally available so addons can share it.
+local Cache = AVY_ItemButtonInfoFrameCache or {}
+AVY_ItemButtonInfoFrameCache = Cache
+
+function filter:Filter(slotData)
+	local itemLink, isBound, _
+	local category = Cache[slotData.itemId] or nil
+
+	if (category) then
+		return L[category]
+	end
+
+	if (C_Container_GetContainerItemInfo) then
+		local containerInfo = C_Container_GetContainerItemInfo(slotData.bag, slotData.slot)
+		if (containerInfo) then
+			itemLink = containerInfo.hyperlink
+			isBound = containerInfo.isBound
+		end
+	else
+		_, _, _, _, _, _, itemLink, _, _, _, isBound = GetContainerItemInfo(slotData.bag, slotData.slot)
+	end
+
+	if (itemLink) then
+		local _, _, itemQuality, _, _, _, _, _, _, _, _, _, _, bindType = GetItemInfo(itemLink)
+
+		-- Only parse items that are Uncommon and above, and are of type BoE or BoU
+		if (itemQuality and itemQuality > 1) and (bindType == 2 or bindType == 3) then
+
+			-- If the item is bound, the bind type is irrelevant.
+			local ignoreItem = false
+			if (isBound) then ignoreItem = true end
+
+			-- GetContainerItemInfo isn't returning 'isBound' in the classics,
+			-- so we need to scan the tooltip the old way here.
+			if (isBound == nil) then
+				Scanner.owner = self
+				Scanner.bag = slotData.bag
+				Scanner.slot = slotData.slot
+				Scanner:SetOwner(self, "ANCHOR_NONE")
+				Scanner:SetBagItem(slotData.bag, slotData.slot)
+				for i = 2,6 do
+					local line = _G[_SCANNER.."TextLeft"..i]
+					if (not line) then
+						break
+					end
+					local msg = line:GetText()
+					if (msg) then
+						if (string_find(msg, S_ITEM_BOP)) then
+							-- item is bound, ignore
+							ignoreItem = true
+							Cache[slotData.itemId] = false
+							break
+						elseif (string_find(msg, S_ITEM_BOA) or string_find(msg, S_ITEM_BOA2) or string_find(msg, S_ITEM_BOA3)) then
+							Cache[slotData.itemId] = S_BOA
+							category = S_BOA
+							break
+						elseif (string_find(msg, S_ITEM_BOE)) then
+							Cache[slotData.itemId] = S_BOE
+							category = S_BOE
+							break
+						else
+							ignoreItem = true
+							Cache[slotData.itemId] = false
+							break
+						end
+					end
+				end
+			end
+
+			-- Only return if not ignored, nil means no filter
+			if (!ignoreItem) then
+				if (category == S_BOE) and filter.db.profile.enableBoE then
+					return L[S_BOE]
+				elseif (category == S_BOA) and filter.db.profile.enableBoA then
+					return L[S_BOA]
+				end
+			end
+		end
+	end
 end
 
-function setFilter:OnDisable()
-  addon:UpdateFilters()
-end
-
-local setNames = {}
-
-function setFilter:Filter(slotData)
-  tooltip = tooltip or create()
-  tooltip:SetOwner(UIParent,"ANCHOR_NONE")
-  tooltip:ClearLines()
-
-if slotData.bag == BANK_CONTAINER then
-	tooltip:SetInventoryItem("player", BankButtonIDToInvSlotID(slotData.slot, nil))
-else
-	tooltip:SetBagItem(slotData.bag, slotData.slot)
-end
-
-  for i = 1,6 do
-    local t = tooltip.leftside[i]:GetText()
-    if self.db.profile.enableBoE and t == ITEM_BIND_ON_EQUIP then
-      return L["BoE"]
-    elseif self.db.profile.enableBoA and (t == ITEM_ACCOUNTBOUND or t == ITEM_BIND_TO_BNETACCOUNT or t == ITEM_BNETACCOUNTBOUND) then
-      return L["BoA"]
-    end
-  end
-  tooltip:Hide()
-end
-
-function setFilter:GetOptions()
-  return {
-    enableBoE = {
-      name = L['Enable BoE'],
-      desc = L['Check this if you want a section for BoE items.'],
-      type = 'toggle',
-      order = 10,
-    },
-    enableBoA = {
-      name = L['Enable BoA'],
-      desc = L['Check this if you want a section for BoA items.'],
-      type = 'toggle',
-      order = 20,
-    },
-  }, addon:GetOptionHandler(self, false, function() return self:Update() end)
+function filter:GetOptions()
+	return {
+		enableBoE = {
+			name = L["Enable BoE"],
+			desc = L["Check this if you want a section for BoE items."],
+			type = "toggle",
+			order = 10,
+		},
+		enableBoA = {
+			name = L["Enable BoA"],
+			desc = L["Check this if you want a section for BoA items."],
+			type = "toggle",
+			order = 20,
+		},
+	}, AdiBags:GetOptionHandler(self, false, function() return self:Update() end)
 end
